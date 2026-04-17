b1fdfb8444a1a31a7c7e482aeb8df77dc5840f3a
sayed <mdsayed.programmer@gmail.com>
2026-04-17 18:18:58 +0600

    final stage

--- friend/src/App.jsx
+++ friend/src/App.jsx
@@ -1,5 +1,5 @@
 import React from 'react';
-import { BrowserRouter as Router, Routes, Route, Link } from 'react-router-dom';
+import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
 import { Toaster } from 'react-hot-toast';
 import { FriendProvider } from './context/FriendContext';
 
@@ -12,35 +12,53 @@ import Home from './pages/Home';
 import FriendDetails from './pages/FriendDetails';
 import Timeline from './pages/Timeline';
 import Stats from './pages/Stats';
+import NotFound from './pages/NotFound'; // Custom 404 Page
 
-// Styles
+// Global Styles
 import './App.css';
-import './media.css';
-
-const NotFound = () => (
-  <div className="error-page">
-    <h1 className="error-code">404</h1>
-    <p className="error-message">Oops! This page doesn't exist.</p>
-    <Link to="/" className="back-home-link">Go back home</Link>
-  </div>
-);
+import './media.css'; // Global Responsiveness
 
 function App() {
   return (
     <FriendProvider>
       <Router>
+        {/* main-layout-wrapper ensures the background and flex-structure are global */}
         <div className="app-wrapper">
-          <Toaster position="top-right" />
+          
+          {/* Feedback notifications for user interactions */}
+          <Toaster 
+            position="top-right" 
+            toastOptions={{
+              duration: 3000,
+              style: {
+                background: '#333',
+                color: '#fff',
+              },
+            }} 
+          />
+
           <Navbar />
+
+          {/* main-content uses flex-grow to push the footer to the bottom */}
           <main className="main-content">
             <Routes>
+              {/* Dashboard / Friend List */}
               <Route path="/" element={<Home />} />
+
+              {/* Individual Friend Profile */}
               <Route path="/friend/:id" element={<FriendDetails />} />
+
+              {/* History of Interactions */}
               <Route path="/timeline" element={<Timeline />} />
+
+              {/* Data Visualization */}
               <Route path="/stats" element={<Stats />} />
+
+              {/* 404 Catch-all: Must be the last Route */}
               <Route path="*" element={<NotFound />} />
             </Routes>
           </main>
+
           <Footer />
         </div>
       </Router>
